de7bbe38b1c1646ff2a8b1ce85c33e65b0f220bd
Dima <126175765+Dima0dev@users.noreply.github.com>
2023-03-27 02:21:08 -0400

    Create BattleHeroBreeder.sol

--- BattleHeroBreeder.sol
+++ BattleHeroBreeder.sol
@@ -31,3 +31,27 @@ contract BattleHeroBreeder is
         require(address(_erc721factory) != address(0), "Setup not correctly");
         _;
     }
+
+    function setGenScience(address genScience) public{ 
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()));
+        _genScience = IBattleHeroGenScience(genScience);
+    }
+    function setERC721Factory(address erc721) public{ 
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()));
+        _erc721factory = IBattleHeroFactory(erc721);
+    }
+    function addBreeder(address breeder) public {
+        require(hasRole(DEFAULT_ADMIN_ROLE, _msgSender()), "Invalid role for add breder");
+        _setupRole(BREED_ROLE, breeder);
+    }
+    function breed(address to, string memory gen) isSetup public returns(uint)  {        
+        require(hasRole(BREED_ROLE, _msgSender()), "Invalid role for breed");
+        uint tokenId = _erc721factory.mint(to, gen);
+        return tokenId;
+    }
+    function breedRandom() isSetup public returns (uint){
+        require(hasRole(BREED_ROLE, _msgSender()), "Invalid role for breed random");
+        string memory randomGen = _genScience.generate();
+        return breed(msg.sender, randomGen);    
+    }
+}
